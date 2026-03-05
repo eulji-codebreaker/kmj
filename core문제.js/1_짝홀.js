d68a4fc8e4101676054cacd27dbe6dda1edc9eaa
als-wl <minnji852@naver.com>
2026-03-05 18:03:41 +0900

    javascript

---
+++ core문제.js/1_짝홀.js
@@ -0,0 +1,11 @@
+function number(num) {
+    if(num %2 == 0){
+        console.log('짝수');
+    }
+    else {
+        console.log('홀수');
+    }
+};
+
+number(2);
+number(5);
---
+++ core문제.js/2.js
@@ -0,0 +1,9 @@
+var person = {
+    firstName: 'GilDong',
+    lastName: 'Hong',
+    age: 20
+};
+
+for(per in person)  {
+    console.log(per, typeof(per));
+}
